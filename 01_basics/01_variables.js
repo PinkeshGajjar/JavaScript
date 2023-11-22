dc0716da9042f51b9fb3fd772c5237c06b7c9129
PinkeshGajjar <46841060+PinkeshGajjar@users.noreply.github.com>
2023-11-22 23:25:49 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "pinkesh@gmail.com"
+var password = "12345"
+accountCity = "Vadodara"
+let accountState;
+
+//accountId = 2
+accountEmail = "test"
+password = "werty"
+accountCity = "Khambhat"
+
+console.log(accountId);
+console.table([accountEmail, password, accountCity, accountState]);
