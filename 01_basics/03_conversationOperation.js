bd9d44fbd9f9a45df4983fd2ca5f46c5d142419b
PinkeshGajjar <46841060+PinkeshGajjar@users.noreply.github.com>
2023-11-25 18:44:56 +0000

    Conversation

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,14 @@
+let score = "33aaa"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+
+console.log(booleanLoggedIn);
